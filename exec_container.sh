dc14a9473f5becc78195994f983f6103e7b5e88f
mohamed-linagora <mohamed.benali91@yahoo.fr>
2021-07-07 14:42:17 +0100

    run_container

---
+++ exec_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Mohamed BEN ALI   --   R&D LINAGORA
+
+echo " ======== exec kaldi container ========= "
+
+docker exec -it docker_kaldi /bin/bash
+
+echo  " ========= execute container ========= "
+
---
+++ run_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Mohamed BEN ALI   --   R&D LINAGORA
+
+echo  " ======= run docker_kaldi  ======== "
+
+docker run -it --name docker_kaldi  kaldi_docker /bin/bash
+
+echo  " ======== container created ======== "
+
